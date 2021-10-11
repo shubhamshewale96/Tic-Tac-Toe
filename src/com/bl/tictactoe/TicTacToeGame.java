bbc2e59662eec262da03fd6ac2ae1e6e25b78a0e
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-11 15:44:08 +0530

    creating method for choose latter X or O

--- src/com/bl/tictactoe/TicTacToeGame.java
+++ src/com/bl/tictactoe/TicTacToeGame.java
@@ -10,10 +10,10 @@ package com.bl.tictactoe;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	/**
-	 * declare 1d array of char type
-	 */
-	static char[] gameBoard = new char[10];
+	char[] gameBoard = new char[10]; //declare 1d array of char type
+	public char Computerletter;
+	public char Playerletter;
+	Scanner scanner = new Scanner(System.in);
 
 	/**
 	 * method for creating empty GameBoard
@@ -23,14 +23,40 @@ public class TicTacToeGame {
 			gameBoard[a] = ' ';
 		}
 	}
+	/**
+     * method for choose Letter X or O
+     */
+	public void selectLetter() {
+        int flag = 0;
+        while (flag == 0) {
+            System.out.print("\n Choose your Letter\n Press 1 -> 'X'\n Press 2 -> 'O'\n Enter your Choice -> ");
+            int choice = scanner.nextInt();
+            switch(choice) {
+            case 1:
+            	Playerletter = 'X';
+                Computerletter = 'O';
+                System.out.println("\n Player letter -> " + Playerletter);
+                System.out.println("Computer letter -> " + Computerletter);
+                flag = 1;
+            	break;
+            case 2:
+            	Playerletter = 'O';
+                Computerletter = 'X';
+                System.out.println("\nPlayer letter -> " + Playerletter);
+                System.out.println("Computer letter -> " + Computerletter);
+                flag = 2;
+                break;
+             default :
+            	 System.out.println("\n plz enter 1 or 2");
+             }
+        }
+	}
 
 	public static void main(String[] args) {
 
 		TicTacToeGame ticTacToeMain = new TicTacToeGame();
-
 		System.out.println("Welcome to TicTacToe Game");
-
 		ticTacToeMain.createGameBoard();
+		ticTacToeMain.selectLetter();
 	}
-
 }
